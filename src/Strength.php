78c6c3996c4891ba38ba44d66f9cc3ca9c4d1f02
Kennedy Tedesco <kennedyt.tw@gmail.com>
2023-11-23 12:21:16 -0300

    Add `binary_operator_spaces` rule

--- src/Strength.php
+++ src/Strength.php
@@ -6,8 +6,8 @@ namespace PhpTui\Cassowary;
 
 final class Strength
 {
-    public const REQUIRED  = 1_001_001_000.0;
-    public const STRONG    = 1_000_000.0;
-    public const MEDIUM    = 1_000.0;
-    public const WEAK      = 1.0;
+    public const REQUIRED = 1_001_001_000.0;
+    public const STRONG = 1_000_000.0;
+    public const MEDIUM = 1_000.0;
+    public const WEAK = 1.0;
 }
